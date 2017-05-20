3d7d1b35d7a0bb67c468c2bcb89b41448c4f841d
Antonio Vivace <puma95.95@gmail.com>
2017-05-20 16:47:18 +0200

    Updated default db and configuration file

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
-// Holds instance-dependent variables
+// This file holds instance-depended settings and configurations
 var config = module.exports = {}
+
 // Talk to @BotFather to set up a bot and get the token
 config.token = '';
+
+// If whitelist_enabled is set to 1, bot will talk only to the chat IDs
+//  specified in the whitelist array.
+config.whitelist_enabled = 0;
+config.whitelist = [];
