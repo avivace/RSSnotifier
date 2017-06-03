15b09d22c5b0e81efe9fe45fc76ae6ab94c2ca1b
Antonio Vivace <puma95.95@gmail.com>
2017-06-03 10:17:55 +0200

    Getting things ready for decent rss reading

--- index.js
+++ index.js
@@ -5,25 +5,19 @@ var request = require('request'),
     schedule = require('node-schedule'),
     sqlite3 = require('sqlite3').verbose(),
     HashMap = require('hashmap'),
-    botUI = require('./bot.js'),
-    config = require('./config.js'),
     stripTags = require('striptags');
-
-const TelegramBot = require('node-telegram-bot-api');
+var botUI = require('./bot.js'),
+    config = require('./config.js'),
+    const TelegramBot = require('node-telegram-bot-api');
 var db = new sqlite3.Database('db.sqlite');
 // Element number
 var i = 0;
-// If an element has the saved head, we should be done and skip on the next
-//  iteration
-var done = 0;
-// Holds the head of the last iteration on every feed
-var alreadyParsed = new HashMap();
-
+var feeds = new HashMap();
+var cachedFeeds = new HashMap;
 // Telegram bot. Polling to fetch new messages
 const bot = new TelegramBot(config.token, {
     polling: true
 });
-
 // Helper Functions
 // Feed Parsing
 function maybeDecompress(res, encoding) {
@@ -66,13 +60,35 @@ function getParams(str) {
     }, {});
     return params;
 }
-
+// Element is in array helper function
+var contains = function(needle) {
+    // Per spec, the way to identify NaN is that it is not equal to itself
+    var findNaN = needle !== needle;
+    var indexOf;
+    if (!findNaN && typeof Array.prototype.indexOf === 'function') {
+        indexOf = Array.prototype.indexOf;
+    } else {
+        indexOf = function(needle) {
+            var i = -1,
+                index = -1;
+            for (i = 0; i < this.length; i++) {
+                var item = this[i];
+                if ((findNaN && item !== item) || item === needle) {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        };
+    }
+    return indexOf.call(this, needle) > -1;
+};
+// contains.call(config.whitelist, chatId))
 // Serious shit
 function getFeeds() {
     // Retrieve all the unique feed URLs, and fetch each of them
     var gf_Query = 'SELECT DISTINCT FeedURL from QUERIES where Active = ?';
     var gf_Query_Params = [1];
-
     db.all(gf_Query, gf_Query_Params, function(error, rows) {
         if (rows.length == 0) console.log("No feeds")
         rows.forEach(function(row) {
@@ -83,38 +99,31 @@ function getFeeds() {
 }
 
 function match(post, queryKeywords, chatId) {
-    // Parse current query element into an Object 
+    // Parse current query element into an Object
     var queryKeywords = JSON.parse(queryKeywords);
     // Declare the match valid in advance
     var matchStillValid = true;
-
     var title = post["rss:title"]["#"];
-
     // For each keyword of current query test the match with Feed Title
     for (var keyword in queryKeywords) {
-
-        // Build a new regular expression with current keyword to pass it 
+        // Build a new regular expression with current keyword to pass it
         //  "i" flag for case-insensitive check
         var re = new RegExp(queryKeywords[keyword], 'i');
-
         // Test the match for current keyword
         var match = title.match(re) ? true : false;
-
-        // If even a single keyword does not pass the test make the entire 
-        //  query useless 
+        // If even a single keyword does not pass the test make the entire
+        //  query useless
         if (match == false) {
             matchStillValid = false;
         }
-
     }
-
-    // Every keyword matched 
+    // Every keyword matched
     if (matchStillValid) {
-        // TODO: prevalid Feed-relative hardcoded values and avoid composing 
+        // TODO: prevalid Feed-relative hardcoded values and avoid composing
         //  the notification message with invalid ones
         console.log("Matched!")
-        // Stripping HTML code from description to avoid Telegram complaining,
-        // it possibly needs addition of allowed tags
+            // Stripping HTML code from description to avoid Telegram complaining,
+            // it possibly needs addition of allowed tags
         var description = stripTags(post["rss:description"]["#"]);
         var link = post["rss:link"]["#"]
         console.log("Matched " + title + ".Sending notification to " + chatId + ".")
@@ -125,31 +134,24 @@ function match(post, queryKeywords, chatId) {
 }
 
 function fetch(url) {
-
     // Watch out for any error raised during fetching/parsing process
     try {
-
         // Stream definitions
         var req = request(url, {
             timeout: 10000,
             pool: false
         });
-
         // Some feeds do not respond without user-agent and accept headers.
         req.setMaxListeners(50);
         req.setHeader('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36');
         req.setHeader('accept', 'text/html,application/xhtml+xml');
-
-
         // HANDLERS
         // Handle request error
         req.on('error', function(err) {
             console.log('ERROR:', err.code, 'on', url, '- Not a valid URL - Aborting feed...\n')
         });
-
         // Handle bad response status codes and, in case of status code 200, go on parsing the feed
         req.on('response', function(res) {
-
             switch (res.statusCode) {
                 case 500:
                     console.log('ERROR: Status Code', res.statusCode, '- INTERNAL SERVER ERROR on', url, '- Aborting feed...\n');
@@ -161,54 +163,29 @@ function fetch(url) {
                     // No request error, fetch the feed and try parsing it!
                     //fetch(url, res);
                     var feedparser = new FeedParser();
-
                     var encoding = res.headers['content-encoding'] || 'identity',
                         charset = getParams(res.headers['content-type'] || '').charset;
                     // res = maybeDecompress(res, encoding);
                     res = maybeTranslate(res, charset);
                     res.pipe(feedparser);
-
-
                     feedparser.on('error', feedParseDone);
                     feedparser.on('end', feedParseDone);
-
                     // Select every active query (and its owner) for current feed
                     var rq_Query = 'SELECT Owner, Keywords AS keywordGroup FROM QUERIES where FeedURL = ? AND Active = ?';
                     var rq_Query_Params = [url, 1];
-
                     db.all(rq_Query, rq_Query_Params, function(error, rows) {
-                        done = 0;
-                        var firsttime = 0;
-                        var title;
-                        i = 0;
                         console.log("Fetching " + url)
-                        feedparser.on('readable', function() {
+                        feedparser.on('readable', function(url) {
                             var post;
-
-                            if (!done) {
-                                while (post = this.read()) {
-                                    title = post["rss:title"]["#"];
-
-                                    if (title == alreadyParsed.get(url)) {
-                                        done = 1;
-                                        alreadyParsed.set(url, firstOfIteration)
-                                        console.log("  Stopping at " + i)
-                                            // just quit this feed
-                                    } else if (i == 0) {
-                                        firstOfIteration = title;
-                                        alreadyParsed.set(url, title)
-                                    }
-
-                                    if (!done) {
-                                        rows.forEach(function(row) {
-                                            match(post, row.keywordGroup, row.Owner);
-                                        });
-                                        i += 1;
-                                    }
-                                }
-                            } else {
-                                feedParseDone("");
+                            console.log(url)
+                            feeds.set(url, new Array());
+                            while (post = this.read) {
+                                feeds.get(url).push(post);
                             }
+                            // doThings(feeds.get(url), cachedFeeds.get(url));
+                            //  rows.forEach(function(row) {
+                            //  match(post, row.keywordGroup, row.Owner);
+                            //  });
                         });
                     });
                     break;
@@ -218,25 +195,21 @@ function fetch(url) {
         console.log('ERROR:', err.message, '- Aborting feed...\n');
     }
 }
-
-// Called on request error, feedparser error or end and manually when we're 
+// Called on request error, feedparser error or end and manually when we're
 //  skipping the feed processing
 function feedParseDone(err) {
     if (err) {
         console.log(err, err.stack);
         return process.exit(1);
     }
-    console.log("  Tried " + i + " matches")
 }
-
 // DO THINGS
 // Bot up and running
 botUI.start(db, bot, config, HashMap)
-// Run the entire thing every 5 seconds
+    // Run the entire thing every 5 seconds
 var job = schedule.scheduleJob('*/5 * * * * *', function() {
     getFeeds();
 });
-
 // TODO: different refresh time for specific feed urls?
 // TODO: Allow user to receive notification as digests in specific time
 // TODO: Some kind of exception handling?
