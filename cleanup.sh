a5afc5ca083803624e1b411a2ca05875190dad5f
avivace <puma95.95@gmail.com>
2017-06-28 22:56:08 +0200

    temp folder

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-mv rssnotifier/db.sqlite temp/db.sqlite
-mv rssnotifier/config.js temp/config.js
+mkdir .tmp
+mv rssnotifier/db.sqlite .tmp/db.sqlite
+mv rssnotifier/config.js .tmp/config.js
 rm -rf rssnotifier
 git clone https://github.com/avivace/rssnotifier
-mv temp/db.sqlite rssnotifier/db.sqlite
-mv temp/config.js rssnotifier/config.js
+mv .tmp/db.sqlite rssnotifier/db.sqlite
+mv .tmp/config.js rssnotifier/config.js
+rm -rf .tmp
 cd rssnotifier
 git update-index --assume-unchanged config.js
 git update-index --assume-unchanged db.sqlite
